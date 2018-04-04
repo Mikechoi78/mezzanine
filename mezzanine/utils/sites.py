Mikechoi78/mezzanine

commit 00f4a63cb10cd27120571c6c84a13f3617ecea78

Made all middleware installation checking consistent.
All check by string first, and then for classes and subclasses,
correctly ignoring things that aren't classes.

mezzanine/utils/sites.py
@@ -9,7 +9,7 @@ from django.contrib.sites.models import Site
 
 from mezzanine.conf import settings
 from mezzanine.core.request import current_request
-from mezzanine.utils.deprecation import get_middleware_setting
+from mezzanine.utils.conf import middlewares_or_subclasses_installed
 
 
 SITE_PERMISSION_MIDDLEWARE = \
@@ -91,7 +91,7 @@ def has_site_permission(user):
     also fall back to an ``is_staff`` check if the middleware is not
     installed, to ease migration.
     """
-    if SITE_PERMISSION_MIDDLEWARE not in get_middleware_setting():
+    if middlewares_or_subclasses_installed([SITE_PERMISSION_MIDDLEWARE]):
         return user.is_staff and user.is_active
     return getattr(user, "has_site_permission", False)
 

mezzanine/core/checks.py
@@ -7,7 +7,7 @@ from django.conf import global_settings
 from django.core.checks import Warning, register
 
 from mezzanine.conf import settings
-from mezzanine.utils.deprecation import get_middleware_setting
+from mezzanine.utils.conf import middlewares_or_subclasses_installed
 from mezzanine.utils.sites import SITE_PERMISSION_MIDDLEWARE
 
 
@@ -159,7 +159,8 @@ def _build_suggested_template_config(settings):
 
 @register()
 def check_sites_middleware(app_configs, **kwargs):
-    if SITE_PERMISSION_MIDDLEWARE not in get_middleware_setting():
+
+    if middlewares_or_subclasses_installed([SITE_PERMISSION_MIDDLEWARE]):
         return [Warning(SITE_PERMISSION_MIDDLEWARE +
                         " missing from settings.MIDDLEWARE - per site"
                         " permissions not applied",

mezzanine/core/middleware.py
@@ -13,6 +13,7 @@ from django.http import (HttpResponse, HttpResponseRedirect,
 from django.middleware.csrf import CsrfViewMiddleware, get_token
 from django.template import Template, RequestContext
 from django.utils.cache import get_max_age
+from django.utils.lru_cache import lru_cache
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
 
@@ -22,8 +23,8 @@ from mezzanine.core.management.commands.createdb import (DEFAULT_USERNAME,
                                                          DEFAULT_PASSWORD)
 from mezzanine.utils.cache import (cache_key_prefix, nevercache_token,
                                    cache_get, cache_set, cache_installed)
-from mezzanine.utils.deprecation import (MiddlewareMixin, is_authenticated,
-                                         get_middleware_setting)
+from mezzanine.utils.conf import middlewares_or_subclasses_installed
+from mezzanine.utils.deprecation import (MiddlewareMixin, is_authenticated)
 from mezzanine.utils.sites import current_site_id
 from mezzanine.utils.urls import next_url
 
@@ -181,12 +182,17 @@ class UpdateCacheMiddleware(MiddlewareMixin):
         # Response needs to be run-through the CSRF middleware again so
         # that if there was a {% csrf_token %} inside of the nevercache
         # the cookie will be correctly set for the the response
-        csrf_mw_name = "django.middleware.csrf.CsrfViewMiddleware"
-        if csrf_mw_name in get_middleware_setting():
+        if csrf_middleware_installed():
             response.csrf_processing_done = False
             csrf_mw = CsrfViewMiddleware()
             csrf_mw.process_response(request, response)
         return response
+
+
+@lru_cache(maxsize=None)
+def csrf_middleware_installed():
+    csrf_mw_name = "django.middleware.csrf.CsrfViewMiddleware"
+    return middlewares_or_subclasses_installed([csrf_mw_name])
 
 
 class FetchFromCacheMiddleware(MiddlewareMixin):
@@ -204,8 +210,7 @@ class FetchFromCacheMiddleware(MiddlewareMixin):
             # We need to force a csrf token here, as new sessions
             # won't receieve one on their first request, with cache
             # middleware running.
-            csrf_mw_name = "django.middleware.csrf.CsrfViewMiddleware"
-            if csrf_mw_name in get_middleware_setting():
+            if csrf_middleware_installed():
                 csrf_mw = CsrfViewMiddleware()
                 csrf_mw.process_view(request, lambda x: None, None, None)
                 get_token(request)

mezzanine/pages/middleware.py
@@ -8,9 +8,8 @@ from mezzanine.conf import settings
 from mezzanine.pages import context_processors, page_processors
 from mezzanine.pages.models import Page
 from mezzanine.pages.views import page as page_view
-from mezzanine.utils.deprecation import (MiddlewareMixin, is_authenticated,
-                                         get_middleware_setting)
-from mezzanine.utils.importing import import_dotted_path
+from mezzanine.utils.conf import middlewares_or_subclasses_installed
+from mezzanine.utils.deprecation import (MiddlewareMixin, is_authenticated)
 from mezzanine.utils.urls import path_to_slug
 
 
@@ -46,21 +45,13 @@ class PageMiddleware(MiddlewareMixin):
         Used in ``mezzanine.pages.views.page`` to ensure
         ``PageMiddleware`` or a subclass has been installed. We cache
         the result on the ``PageMiddleware._installed`` to only run
-        this once. Short path is to just check for the dotted path to
-        ``PageMiddleware`` in ``MIDDLEWARE_CLASSES`` - if not found,
-        we need to load each middleware class to match a subclass.
+        this once.
         """
         try:
             return cls._installed
         except AttributeError:
             name = "mezzanine.pages.middleware.PageMiddleware"
-            mw_setting = get_middleware_setting()
-            installed = name in mw_setting
-            if not installed:
-                for name in mw_setting:
-                    if issubclass(import_dotted_path(name), cls):
-                        installed = True
-                        break
+            installed = middlewares_or_subclasses_installed([name])
             setattr(cls, "_installed", installed)
             return installed
 

mezzanine/utils/conf.py
@@ -248,8 +248,12 @@ def real_project_name(project_name):
 
 
 def middlewares_or_subclasses_installed(needed_middlewares):
-
+    """
+    When passed an iterable of dotted strings, returns True if all
+    of the middlewares (or their subclasses) are installed.
+    """
     middleware_setting = set(get_middleware_setting())
+
     # Shortcut case, check by string
     if all(m in middleware_setting for m in needed_middlewares):
         return True

commit 99bb158bbd80bc81808075df876980e131860e96

Fix sense of other check for SITE_PERMISSION_MIDDLEWARE.
Issue introduced by commit 00f4a63.

mezzanine/utils/sites.py
@@ -91,7 +91,7 @@ def has_site_permission(user):
     also fall back to an ``is_staff`` check if the middleware is not
     installed, to ease migration.
     """
-    if middlewares_or_subclasses_installed([SITE_PERMISSION_MIDDLEWARE]):
+    if not middlewares_or_subclasses_installed([SITE_PERMISSION_MIDDLEWARE]):
         return user.is_staff and user.is_active
     return getattr(user, "has_site_permission", False)
 